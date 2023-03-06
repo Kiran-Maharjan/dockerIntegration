9b46edd2c3593c3be38d3ca6f0febd29bb736df3
Kiran-Maharjan <kiranmaharjan8888@gmail.com>
2023-03-06 13:20:33 +0545

    first upload

---
+++ dockerIntegration/src/test/java/com/kiran/dockerIntegration/dockerIntegration/DockerIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiran.dockerIntegration.dockerIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
